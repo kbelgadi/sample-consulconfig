014ba06db7f142cffeb9cd1aa6f931200ebb8360
Kamel Belgadi <kambelga@publicisgroupe.net>
2019-01-31 11:03:01 +0100

    update: change app name in k8s

--- target/dockerfile/run.sh
+++ target/dockerfile/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting Sample Application"
 echo "********************************************************"
-java -jar /usr/local/licensingservice/sample-consulconfig-1.0.1-SNAPSHOT.jar
+java -jar /usr/local/licensingservice/sample-consulconfig-0.0.1-SNAPSHOT.jar
